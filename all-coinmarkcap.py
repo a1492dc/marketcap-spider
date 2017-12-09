ef75dd3edb2bdfc0ad53d11272849bae8281fa8d
a1492dc <a1492dc@gmail.com>
2017-12-09 01:22:34 +0000

    Add files via upload
    added other general attibutes per coin

--- all-coinmarkcap.py
+++ all-coinmarkcap.py
@@ -4,9 +4,13 @@ import scrapy
 class coin_item(scrapy.Item):
     coin_url = scrapy.Field()
     coin_name = scrapy.Field()
+    coin_code = scrapy.Field()
+    coin_markcap = scrapy.Field()
+    coin_price = scrapy.Field()
+    coin_volume24 = scrapy.Field()
 
 
-class QuotesSpider(scrapy.Spider):
+class Spider(scrapy.Spider):
     name = "all-coinmarkcap"
 
     def start_requests(self):
@@ -20,6 +24,12 @@ class QuotesSpider(scrapy.Spider):
         item = []
 	for coin in response.css("table#currencies-all tr"):
 		item = coin_item()
-		item["coin_name"] = coin.css("td span.currencies-symbol a::text").extract_first(),
-		item["coin_url"] = coin.css("td span.currency-symbol a::attr(href)").extract(),
+		item["coin_url"] = coin.css('td span.currency-symbol a::attr(href)').extract(),
+		item["coin_name"] = coin.css('td a.currency-name-container::text').extract(),
+		item["coin_code"] = coin.css('td span.currency-symbol a::text').extract(),
+		item["coin_markcap"] = coin.css('td.market-cap::attr(data-usd)').extract(),
+		item["coin_price"] = coin.css('td a.price::attr(data-usd)').extract(),
+		item["coin_volume24"] = coin.css('td.circulating-supply a::attr(data-supply)').extract(),
+
+
 		yield item
